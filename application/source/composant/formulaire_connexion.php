e3e3bcb8d925f9813cb4c80c0cc825c9835d1042
Sofian Mattheus <mattheussofian1@gmail.com>
2021-11-24 16:23:03 +0100

    coul_bout

--- application/source/composant/formulaire_connexion.php
+++ application/source/composant/formulaire_connexion.php
@@ -1,9 +1,11 @@
 
     
-<main class="form-signin">
-  <form>
-    <img class="mb-4" src="./media/pop.png" alt="" width="72" height="57">
+<div class=container-fluid> 
+  <main class="form-signin">
+  <form>  
+    <img class="mb-4" src="./media/pop.png" alt="" width="70" height="72" class="mx-auto d-block">
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+</div>
 
     <div class="form-floating">
       <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
