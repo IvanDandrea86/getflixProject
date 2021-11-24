57fc6440f538bd3f5a800e8f4391cc8c3b2a2635
Sofian Mattheus <mattheussofian1@gmail.com>
2021-11-24 10:44:31 +0100

    media addeds

--- application/source/index.php
+++ application/source/index.php
@@ -5,20 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./css/style.css">
-    
+     <link rel="stylesheet" href="./css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
      <title>Getflix</title>
 </head>
+<body>
+    
 
 <?php
 include ("./composant/header.php");
 include ("./composant/body.php");
 include ("./composant/footer.php");
 include ("./composant/categorie_films.php");
+
 include ("./composant/formulaire_inscription.php");
 include ("./composant/formulaire_connexion.php");
+include ("./composant/carroussel.php");
 ?>
+</body>
+</html>
 
 
 
