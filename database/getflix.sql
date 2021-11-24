0768aa84a8d3f183710a23297e3182693a6f2fe6
annejungers <annejungers@hotmail.com>
2021-11-24 09:27:05 +0100

    getflix

--- database/getflix.sql
+++ database/getflix.sql
@@ -1,12 +1,56 @@
 
-CREATE DATABASE IF NOT EXISTS getflix;
-USE getflix;
+
+
+
+CREATE DATABASE IF NOT EXISTS GetFlix;
+USE GetFlix;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+01:00";
+
+
+-- Structure of the "Users" table
 
 CREATE TABLE `Users` (
-  `PersonID` binary(36) NOT NULL PRIMARY KEY,
-  `Username` varchar(255) NOT NULL,
+  `Id` binary(18) NOT NULL PRIMARY KEY,
+  `Username` varchar(255) NOT NULL UNIQUE,
   `Email` varchar(100) NOT NULL UNIQUE,
   `Password` varchar(255) NOT NULL,
   `LastName` varchar(255) DEFAULT NULL,
-  `FirstName` varchar(255) DEFAULT NULL
-) 
+  `FirstName` varchar(255) DEFAULT NULL,
+  `Registration` smalldatetime,
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+
+-- Loading the data from the Users table
+
+INSERT INTO `users` (`Id`, `Username`, `Email`, `Password`, `Lastname`, `Firstname`, `Registration` ) VALUES
+(UUID(), 'tigercub++', 'annejungers@hotmail.com', '$2y$10$QJq6//fZZeUhrcedi8FSB.RPfrprMXYdFX4AAauCK7AWBdgwFwhea', 'Anne', 'Jungers'),
+(UUID(), 'nikobackend', 'gael.layeux@gmail.com', '$2y$10$tyhyBwdXMqJ3DwZPebNwieDnA7IURCkV6FPsKkzqKm2vhGBIwB6re','nicolas', 'valiacas'),
+(UUID(), 'ivanthemanager', 'nicolas.valiacas@icloud.com', '$2y$10$g9SIsLwdyWcjk85THo3xtO4TscnHhmUyaaQpmUeY68rLFQMDtkxju', 'ivan', 'dandrea'),
+(UUID(), 'sofianfrontend', 'sofian.mattheus@gmail.com', '$2y$10$tyhyBwdXMqJ3DwZPebNwieDnA7IURCkV6FPsKkzqKm2vhGBIwB6re', 'sofian','mattheus');
+
+--
+-- Structure of the "comments" table
+--
+
+CREATE TABLE `comments` (
+  `Id_comments` binary(36) NOT NULL PRIMARY KEY,
+  `Comment` text NOT NULL,
+  `Rating` int(5) NOT NULL,
+  `Movie_Id` int(10) NOT NULL,
+  `UserId` binary(18) NOT NULL FOREIGN KEY
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Loading data for the "comments" table
+--
+/*
+INSERT INTO `comments` (`Id_comments`, `Comment`, `Rating`, `Movie_Id`, `UserId`) VALUES
+(UUID_SHORT(), 'Excellent film. I feel now so in love with my boyfriend for making me watch this!', 5, 15, 15),
+(UUID_SHORT(), 'Absolutely horrifying! Looks like it was filmed in the 70s.', 1, 14, 2);
+(UUID_SHORT(), 'Excellent if you want to learn how to kill people.', 3, 11, 10);
+
+*/
+
