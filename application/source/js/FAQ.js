8bb74d3c49bbc54451adac4c1f2e87c809d0f5fd
Sofian Mattheus <mattheussofian1@gmail.com>
2021-11-30 18:10:39 +0100

    fin du mardi

---
+++ application/source/js/FAQ.js
@@ -0,0 +1,15 @@
+const allCross = document.querySelectorAll('.visible-panel img');
+
+console.log(allCross);
+
+
+allCross.forEach(element => {
+
+        element.addEventListener('click', function(){
+
+            if(this.scr.includes('croix')){
+                this.scr = './media/minus.svg';
+            } else if (this.scr.includes('minus'))
+                this.scr = './media/croix.svg';
+        })
+});
