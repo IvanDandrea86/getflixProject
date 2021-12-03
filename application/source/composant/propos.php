3e83c563fc5aeb8074e7e956e407408707575821
Sofian Mattheus <mattheussofian1@gmail.com>
2021-12-03 10:29:19 +0100

    vendredi matin

--- application/source/composant/propos.php
+++ application/source/composant/propos.php
@@ -1,3 +1,16 @@
-<h1>L équipe de choc</h1>
-
-<img src="./composant/media/groupe1.JPG" alt="Le groupe">
+<div class="final d-flex align-self-center">
+    <div class="finaal align-self-center">
+    
+        <h1 id="final">L'équipe de <strong style="color:red">choc</strong>,<strong style="color:orange">de folie</strong>,<strong style="color:green">de la mort <strong style="color:yellow">qui tue</strong></h1>
+        
+        <div class="nous"> 
+            <a href="/media/groupe1.JPG" class="img-fluid" id="a">Qui a créé ce sublime site?</a>
+            <br>
+            <a href="/media/groupe3.JPG" class="img-fluid" id="b">Qui est la future équipe pro du front et back? </a>
+            <br>
+            <a href="/media/groupe5.JPG" class="img-fluid" id="c">A combien on a travailler?</a>
+            <br>
+            <a href="/media/groupe7.JPG" class="img-fluid" id="d">Qui a le plus beau sourire?</a>
+        </div>
+    </div>
+</div>
